bc2e65b6b62b50a5509dae7dfc33ea1da9079870
Luciano <canalmeuovo36@gmail.com>
2022-05-12 23:12:15 -0300

    upp

--- index.js
+++ index.js
@@ -3,8 +3,15 @@ export default function(){
     function teste(str){
         return str + ' olá mundo!'
     }
+    function somar(a,b){
+        return a + b;
+    }
+    function dividir(valor,dividir){
+        return valor / dividir;
+    }
+    
 
 
 
-return {teste}
+return {teste,somar,dividir}
 }
